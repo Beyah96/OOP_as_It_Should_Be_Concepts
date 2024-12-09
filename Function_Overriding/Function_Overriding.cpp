a3ee4cbcc42d56c003269c9654c59ba8e673b00a
Beyah96 <bahsaid96@gmail.com>
2024-12-09 23:58:47 +0100

    adding a new section on function overriding in OOP in c++

---
+++ Function_Overriding/Function_Overriding.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+using namespace std;
+
+class clsPersonInfo {
+private:
+	short static _ID;
+	string _FirstName, _LastName, _FullName, _Email, _Phone;
+public:
+	// ID
+	short GetID() {
+		return _ID++;
+	}
+
+
+	// First Name
+	string GetFirstName() {
+		return _FirstName;
+	}
+	void SetFirstName(string FirstName) {
+		_FirstName = FirstName;
+	}
+
+	// Last Name
+	string GetLastName() {
+		return _LastName;
+	}
+	void SetLastName(string LastName) {
+		_LastName = LastName;
+	}
+
+	// Email
+	string GetEmail() {
+		return _Email;
+	}
+	void SetEmail(string Email) {
+		_Email = Email;
+
+	}
+
+	// Phone
+	string GetPhone() {
+		return _Phone;
+	}
+	void SetPhone(string Phone) {
+		_Phone = Phone;
+
+	}
+
+	// FullName
+	string GetFullName() {
+		return FirstName + " " + LastName;
+	}
+	_declspec(property(get = GetFirstName, put = SetFirstName)) string FirstName;
+	_declspec(property(get = GetLastName, put = SetLastName)) string LastName;
+	_declspec(property(get = GetEmail, put = SetEmail)) string Email;
+	_declspec(property(get = GetPhone, put = SetPhone)) string Phone;
+
+	clsPersonInfo(string FirstName, string LastName, string Email, string Phone) {
+		SetFirstName(FirstName);
+		SetLastName(LastName);
+		SetEmail(Email);
+		SetPhone(Phone);
+
+	}
+	void Print() {
+		cout << "info : " << endl;
+		cout << "*****************************************";
+
+		cout << endl << "ID         : " << GetID();
+		cout << endl << "First Name : " << GetFirstName();
+		cout << endl << "Last Name  : " << GetLastName();
+		cout << endl << "Full Name  : " << GetFullName();
+		cout << endl << "Email      : " << GetEmail();
+		cout << endl << "Phone      : " << GetPhone();
+		cout << endl << "*****************************************" << endl << endl;
+	}
+
+	void SendEmail(string Subject, string Body) {
+		cout << "The following message sent successfully to email : " << _Email << endl;
+		cout << "Subject : " << Subject << endl;
+		cout << "Body : " << Body << endl << endl;
+	}
+	void SendSMS(string SMS) {
+		cout << "The following SMS sent successfully to phone : " << _Phone << endl;
+		cout << "Subject : " << SMS << endl;
+	}
+
+};
+
+class clsEmployeeInfo : public clsPersonInfo {
+private:
+	string _Title, _Departement;
+	int _Salary;
+public:
+	clsEmployeeInfo(string FirstName, string LastName, string Email, string Phone, string Title, string Departement, int Salary)
+		: clsPersonInfo(FirstName, LastName, Email, Phone) {
+		_Title = Title;
+		_Departement = Departement;
+		_Salary = Salary;
+	}
+	void setTitle(string Title) {
+		_Title = Title;
+	}
+	string getTitle() {
+		return _Title;
+	}
+
+	void setDepartement(string Departement) {
+		_Departement = Departement;
+	}
+	string getDepartement() {
+		return _Departement;
+	}
+
+	void setSalary(int Salary) {
+		_Salary = Salary;
+	}
+	int getSalary() {
+		return _Salary;
+	}
+	/*
+	void Print() {
+		clsPersonInfo::Print();
+		cout << "Departement : " << getDepartement() << endl;
+		cout << "Title : " << getTitle() << endl;
+		cout << "salary : " << getSalary();
+	}
+	*/
+	void Print() {
+		cout << "info : " << endl;
+		cout << "*****************************************";
+
+		cout << endl << "ID          : " << GetID();
+		cout << endl << "First Name  : " << GetFirstName();
+		cout << endl << "Last Name   : " << GetLastName();
+		cout << endl << "Full Name   : " << GetFullName();
+		cout << endl << "Email       : " << GetEmail();
+		cout << endl << "Phone       : " << GetPhone();
+		cout << endl << "Departement : " << getDepartement();
+		cout << endl << "Title       : " << getTitle();
+		cout << endl << "salary      : " << getSalary();
+		cout << endl << "*****************************************" << endl << endl;
+	}
+};
+
+short clsPersonInfo::_ID = 0;
+
+int main() {
+	clsEmployeeInfo Employee("Mohamed", "Yacine", "Lamin@Lamin.com", "0352467468", "CEO", "Signusk", 7000);
+	Employee.Print();
+	
+	system("pause>0");
+	return 0;
+}
