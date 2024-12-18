b601e4ba32d8796e01b6813371f0eadee0cc7d73
Beyah96 <bahsaid96@gmail.com>
2024-12-18 22:32:55 +0100

    Adding some functions like : putting first letter in upper/lower case, inverting string's letters cases, Lower all string's letters or upper all string's letters

--- String_Library_Project/String_Library_Project.cpp
+++ String_Library_Project/String_Library_Project.cpp
@@ -4,11 +4,7 @@ using namespace std;
 
 int main() {
 	clsString String1;
-	cout << endl << String1.GetValue() << endl;
-	String1.Value = "Mohamed";
-	cout << endl << String1.GetValue() << endl;
-	cout << endl << String1.Length() << endl;
-	cout << endl << String1.Length("Bah") << endl;
+
 	system("pause>0");
 	return 0;
 }
@@ -19,23 +15,16 @@ int main() {
 //CountSmallLetters
 //CountSpecificLetters
 //CountVowels
-//CountWords
-//GetValue
 //InvertAllLettersCase
 //InvertLetterCase
 //IsVowel
 //JoinString
-//Length
 //LowerAllString
-//LowerFirstLetterOfEachWord
 //RemovePunctuation
 //ReplaceWord
 //ReverseWordsInString
-//SetValue
 //SmallLetters
 //Split
 //Trim
 //TrimLeft
 //TrimRight
-//UpperFirstLetterOfEachWord
-//Value
