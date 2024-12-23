8bdce22e82344a901cb293e42dfa3181c418c25c
Beyah96 <bahsaid96@gmail.com>
2024-12-23 22:54:15 +0100

    Adding a new project : Date Library Project

---
+++ Date_Library_Project/Date_Library_Project.cpp
@@ -0,0 +1,11 @@
+#pragma warning (disable : 4996)
+#include <iostream>
+#include "clsDate.h";
+using namespace std;
+
+int main() {
+	clsDate Date;
+	Date.Print();
+	system("pause>0");
+	return 0;
+}
---
+++ Date_Library_Project/clsDate.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+class clsDate
+{
+private:
+	short _Day;
+	short _Month;
+	short _Year;
+public:
+	clsDate() {
+		time_t t = time(NULL);
+		tm* now = localtime(&t);
+		this->_Day = now->tm_mday;
+		this->_Month = now->tm_mon;
+		this->_Year = now->tm_year + 1900;
+	}
+	void Print() {
+		cout << "Day   : " << _Day << endl;
+		cout << "Month : " << _Month << endl;
+		cout << "Year  : " << _Year << endl;
+	}
+};
+
